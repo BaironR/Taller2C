ca163e44cb925362dc0d36f4394caf45682dfbf2
BaironR <blrojas2668@gmail.com>
2023-11-01 23:34:39 -0300

    commit 13: Agregado metodo de extraccion de paquetes, y destructor en heap

--- Taller2C/Heap.cpp
+++ Taller2C/Heap.cpp
@@ -7,8 +7,10 @@ Heap::Heap(int tamanio_maximo)
 	arreglo_nodos = new NodoHeap* [tamanio_maximo];
 }
 
-Heap::~Heap()
-{
+Heap::~Heap(){
+	for (int i = 0; i < this->cantidad_actual; i++) {
+		delete arreglo_nodos[i];
+	}
 }
 
 void Heap::insertar_paquete(Paquete* paquete_agregar)
@@ -17,9 +19,13 @@ void Heap::insertar_paquete(Paquete* paquete_agregar)
 		return;
 	}
 
+	// Se inserta el paquete al final del arreglo
 	this->cantidad_actual++;
 	int indice = cantidad_actual - 1;
 	arreglo_nodos[indice] = new NodoHeap(paquete_agregar);
+
+	/* Se modifica el min heap en caso de no cumplirse la condición de heap
+	Mientras el indice no sea 0, y el tiempo de entrega del paquete del padre sea mayor que el de su hijo, se intercambia padre con hijo */
 
 	while (indice != 0 && arreglo_nodos[get_padre(indice)]->get_paquete()->get_tiempo_entrega() > arreglo_nodos[indice]->get_paquete()->get_tiempo_entrega()) {
 		std::swap(arreglo_nodos[indice],arreglo_nodos[get_padre(indice)]);
@@ -27,14 +33,10 @@ void Heap::insertar_paquete(Paquete* paquete_agregar)
 	}
 }
 
-void Heap::eliminar_paquete(int indice)
-{
-}
-
 void Heap::heapify_min(int indice)
 {
-	int izquierdo = 2 * indice;
-	int derecho = 2 * indice + 1;
+	int izquierdo = get_hijo_izq(indice);
+	int derecho = get_hijo_der(indice);
 	int mas_pequenio = indice;
 
 	if (izquierdo < cantidad_actual && arreglo_nodos[izquierdo]->get_paquete()->get_tiempo_entrega() < arreglo_nodos[mas_pequenio]->get_paquete()->get_tiempo_entrega()) {
@@ -51,14 +53,25 @@ void Heap::heapify_min(int indice)
 	}
 }
 
-Paquete* Heap::retornar_paquete()
-{
-	return nullptr;
-}
-
 Paquete* Heap::extraer_paquete()
 {
-	return nullptr;
+	if (cantidad_actual <= 0) {
+		return nullptr;
+	}
+
+	if (cantidad_actual == 1) {
+		this->cantidad_actual--;
+		Paquete* paquete_raiz = arreglo_nodos[0]->get_paquete();
+		delete arreglo_nodos[0];
+		return paquete_raiz;
+	}
+
+	Paquete* paquete_raiz = arreglo_nodos[0]->get_paquete();
+	delete arreglo_nodos[0];
+	arreglo_nodos[0] = arreglo_nodos[cantidad_actual-1];
+	this->cantidad_actual--;
+	heapify_min(0);
+	return paquete_raiz;
 }
 
 int Heap::get_padre(int indice)
@@ -79,7 +92,6 @@ int Heap::get_hijo_der(int indice)
 void Heap::imprimir_arreglo() {
 	for (int i = 0; i < this->cantidad_actual; i++) {
 		Paquete* paquete = arreglo_nodos[i]->get_paquete();
-
 		std::cout << paquete->get_codigo_aduana() << "-" << paquete->get_contenido_fragil() << "-" << paquete->get_tiempo_entrega() << std::endl;
 	}
 }
--- Taller2C/Heap.h
+++ Taller2C/Heap.h
@@ -13,9 +13,7 @@ public:
 	Heap(int tamanio_maximo);
 	~Heap();
 	void insertar_paquete(Paquete* paquete_agregar);
-	void eliminar_paquete(int indice);
 	void heapify_min(int indice);
-	Paquete* retornar_paquete();
 	Paquete* extraer_paquete();
 	int get_padre(int indice);
 	int get_hijo_izq(int indice);
